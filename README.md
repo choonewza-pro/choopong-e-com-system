d12d00675d3dcf00c9bf658c770144c979b1c032
choonewza-pro <choonewza@gmail.com>
2025-05-21 14:15:13 +0700

    update label cart button

--- README.md
+++ README.md
@@ -1,11 +1,30 @@
 ## สร้าง Branche ชื่อ login-page-feature
 
 ```bash
-git checkout -b "login-page-feature"
+git checkout -b login-page-feature
 
 git add .
 
 git commit -m "add login page"
 
 git push -u origin login-page-feature
+```
+
+## กลับไปที่ main
+
+```bash
+git checkout main
+git pull
+```
+
+## สร้าง Branche ชื่อ update label cart button
+
+```bash
+git checkout -b update-label-cart-button
+
+git add .
+
+git commit -m "update label cart button"
+
+git push -u origin update-label-cart-button
 ```
--- src/App.tsx
+++ src/App.tsx
@@ -16,18 +16,13 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>E-Com System 1.0.0 (add login page)</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
+      <h1>E-Com System 1.0.0</h1>
+      <div>
+        <ul>
+          <li>Add login page</li>
+          <li>Update label cart button</li>
+        </ul>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
